0443f1e8c22dc7df7d2d9a4a0caf4a0bdafd95bb
viniciuslemos2102 <viniciuslemoscontato21@gmail.com>
2024-01-09 17:58:06 -0300

    Adicionar arquivos de projeto.

---
+++ Controllers/AddCustumerController.cs
@@ -0,0 +1,27 @@
+﻿using Domain.Contracts.UseCases.AddCustumer;
+using Microsoft.AspNetCore.Mvc;
+using WebApi.Models.AddCustumer;
+
+namespace WebApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AddCustumerController : ControllerBase
+    {
+        private readonly IAddCustumerUseCase _addCustumerUseCase;
+
+        public AddCustumerController(IAddCustumerUseCase addCustumerUseCase)
+        {
+            _addCustumerUseCase = addCustumerUseCase;
+        }
+
+        [HttpPost]
+        public IActionResult AddCustumer(AddCustumerInput input)
+        {
+            var custumer = new Domain.Entities.Custumer(input.Name, input.Email, input.Document);
+            _addCustumerUseCase.AddCustumer(custumer);
+            return Created("", custumer);
+
+        }
+    }
+}
---
+++ Models/AddCustumer/AddCustumerInput.cs
@@ -0,0 +1,9 @@
+﻿namespace WebApi.Models.AddCustumer
+{
+    public class AddCustumerInput
+    {
+        public string Name { get; set; } =  string.Empty;
+        public string Email { get; set; } = string.Empty;
+        public string Document { get; set; } = string.Empty;
+    }
+}
